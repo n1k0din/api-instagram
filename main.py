2ce4073c4dc6410275237d5951408b89e8fab16b
n1k0din <nik726@gmail.com>
2021-04-14 18:45:38 +0700

    added download_latest_launch_photos func

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from pathlib import Path
+from pprint import pprint
 
 
 IMAGES_DIR = 'images/'
@@ -15,15 +16,27 @@ def download_img(url, filename):
         file.write(response.content)
 
 
+def get_latest_launch_photo_urls():
+    url = 'https://api.spacexdata.com/v4/launches/latest'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return response.json()['links']['flickr']['original']
+
+
+def download_latest_launch_photos(photo_urls):
+    filename_template = 'spacex{}.jpg'
+
+    for num, photo_url in enumerate(photo_urls, start=1):
+        filename = filename_template.format(num)
+        download_img(photo_url, filename)
+
+
 def main():
     Path(IMAGES_DIR).mkdir(parents=True, exist_ok=True)
 
-    filename = 'hubble.jpg'
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-
-    download_img(url, filename)
-
-
+    photo_urls = get_latest_launch_photo_urls()
+    download_latest_launch_photos(photo_urls)
 
 
 if __name__ == '__main__':
