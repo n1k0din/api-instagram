39b9c0d558cec926d680f33e920bff5382511320
n1k0din <nik726@gmail.com>
2021-04-15 15:44:53 +0700

    added hubble images downloader

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import requests
+import os.path
 from pathlib import Path
+from urllib.parse import unquote, urlsplit
+
+import requests
 
 
 IMAGES_DIR = 'images/'
@@ -8,7 +11,7 @@ IMAGES_DIR = 'images/'
 def download_img(url, filename):
     full_path = f'{IMAGES_DIR}{filename}'
 
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     response.raise_for_status()
 
     with open(full_path, 'wb') as file:
@@ -33,8 +36,8 @@ def fetch_spacex_last_launch():
         download_img(photo_url, filename)
 
 
-def get_hubble_image_urls(id=1):
-    url = f'http://hubblesite.org/api/v3/image/{id}'
+def get_hubble_image_urls(image_id):
+    url = f'http://hubblesite.org/api/v3/image/{image_id}'
     response = requests.get(url)
     response.raise_for_status()
 
@@ -42,12 +45,28 @@ def get_hubble_image_urls(id=1):
     return [f'https:{image_file["file_url"]}' for image_file in image_files]
 
 
+def download_hubble_image(image_id):
+    image_url = get_hubble_image_urls(image_id)[::-1]
+    ext = get_file_extension(image_url)
+    filename = f'{image_id}{ext}'
+    download_img(image_url, filename)
+
+
+def get_file_extension(file_url):
+    unquoted_url = unquote(file_url)
+    _, _, path, _, _ = urlsplit(unquoted_url)
+    _, tail = os.path.split(path)
+    _, ext = os.path.splitext(tail)
+
+    return ext
+
+
 def main():
     Path(IMAGES_DIR).mkdir(parents=True, exist_ok=True)
 
     fetch_spacex_last_launch()
 
-    print(get_hubble_image_urls())
+    download_hubble_image(2)
 
 
 if __name__ == '__main__':
